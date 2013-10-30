31d26951699c9370a447f90d70f3590679dd039b
Nacho Facello <nacho@nucleartesuji.com>
2013-10-30 18:35:15 -0200

    CardDisplayActivity allows you to browse cards

--- src/com/nucleartesuji/powerstruggleeventplanner/CardDisplayActivity.java
+++ src/com/nucleartesuji/powerstruggleeventplanner/CardDisplayActivity.java
@@ -1,15 +1,28 @@
 package com.nucleartesuji.powerstruggleeventplanner;
 
+import java.util.List;
+
+import com.nucleartesuji.powerstruggleeventplanner.game.Card;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Toast;
 
 public class CardDisplayActivity extends Activity {
+	private List<Card> cards;
+	private int position;
 
+	@SuppressWarnings("unchecked")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_card_display);
+		
+		cards = (List<Card>) getIntent().getSerializableExtra(EventSortActivity.CARDS);
+		position = 0;
+		updateCardArea();
 	}
 
 	@Override
@@ -18,5 +31,34 @@ public class CardDisplayActivity extends Activity {
 		getMenuInflater().inflate(R.menu.card_display, menu);
 		return true;
 	}
+	
+	public void onNewDeckClick(View button){
+	}
+	
+	public void onNextCardClick(View button){
+		if (position < cards.size()-1) {
+			position++;
+			updateCardArea();
+		} else {
+			Toast.makeText(this, "No more cards", Toast.LENGTH_SHORT).show();
+		}
+	}
+	
+	public void onPreviousCardClick(View button){
+		if (position > 0) {
+			position--;
+			updateCardArea();
+		} else {
+			Toast.makeText(this, "No more cards", Toast.LENGTH_SHORT).show();
+		}
+	}
 
+	private void updateCardArea() {
+		
+		Card card = cards.get(position);
+		
+		View cardArea = findViewById(R.id.cardArea);
+		
+		new CardDisplayer(cardArea).display(card);
+	}
 }
